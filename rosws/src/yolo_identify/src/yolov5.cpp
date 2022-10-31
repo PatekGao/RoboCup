f3cdd52e11f50328a932e513f275a51c2030f917
PatekGao <1315587032@qq.com>
2022-10-31 14:57:14 +0800

    [milestone] An elegant yolov7 version.

--- rosws/src/yolo_identify/src/yolov5.cpp
+++ rosws/src/yolo_identify/src/yolov5.cpp
@@ -23,23 +23,17 @@
 #include "rc_msgs/results.h"
 #include "rc_msgs/point.h"
 #include "std_msgs/Bool.h"
-//msgs  //yolov5
-//yolov5//yolov7
+//msgs  //yolov7
 #include "yolo/yolo.hpp"
 
 using namespace std;
 
-// deleted
-// deleted
-
-//new
 shared_ptr<Yolo::Infer> engine;
 const auto using_mode = Yolo::Mode::FP16;   // 注意：修改此选项后必须删除原trtmodel才会生效
 const int DEVICE = 0;
 const int BATCH_SIZE = 1;
 const float CONF_THRESH = .5;
 const float NMS_THRESH = .4;
-#define MAX_IMAGE_INPUT_SIZE_THRESH (3000 * 3000) // ensure it exceed the maximum size in the input images !
 
 static const char *cocolabels[] = {
         "Tissues", "Toilet Rolls", "Soap", "Bottled Hand Sanitizer",
@@ -116,7 +110,7 @@ inline long getNowTime() {
     return tv.tv_sec;
 }
 
-cv::Mat inference(const cv::Mat& image) {
+cv::Mat inference(const cv::Mat &image) {
     assert(engine != nullptr);
 
     shared_future<Yolo::BoxArray> box;
@@ -138,20 +132,12 @@ cv::Mat inference(const cv::Mat& image) {
     }
     return image;
 }
-//new
 
 std::atomic<bool> beatRun(true);
 int step = 0;
 bool isIdentify = false;
 ros::Publisher resPub;
 ros::Publisher beatPub;
-float *buffers[2];
-uint8_t *img_host = nullptr;
-uint8_t *img_device = nullptr;
-//IExecutionContext *context;
-//float prob[BATCH_SIZE * OUTPUT_SIZE];
-//int inputIndex;
-
 
 void imageCallback(const sensor_msgs::ImageConstPtr &msg) {
     if (!isIdentify) {
@@ -192,57 +178,14 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg) {
         ps[3].y = obj.bottom;
         tmp.contours = ps;
         auto caption = cv::format("%s %.2f", name, obj.confidence);
-        int width = cv::getTextSize(caption, 0, 1, 2, nullptr).width + 10;
+        int width = cv::getTextSize(caption, 0, 1, 1, nullptr).width + 10;
         cv::rectangle(img, cv::Point(obj.left - 3, obj.top - 33), cv::Point(obj.left + width, obj.top),
                       cv::Scalar(b, g, r), -1);
-        cv::putText(img, caption, cv::Point(obj.left, obj.top - 5), 0, 1, cv::Scalar::all(0), 2, 16);
+        cv::putText(img, caption, cv::Point(obj.left, obj.top - 5), 0, 1, cv::Scalar::all(0), 1, 16);
         Result.results.emplace_back(tmp);
     }
-    Result.color = *(cv_bridge::CvImage(std_msgs::Header(),"bgr8", img).toImageMsg());
+    Result.color = *(cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg());
     resPub.publish(Result);
-//copy to memory and interface for v5
-/*
-    float *buffer_idx = (float *) buffers[inputIndex];
-    size_t size_image = img.cols * img.rows * 3;
-    size_t size_image_dst = INPUT_H * INPUT_W * 3;
-    //copy data to pinned memory
-    memcpy(img_host, img.data, size_image);
-    //copy data to DEVICE memory
-    CUDA_CHECK(cudaMemcpyAsync(img_device, img_host, size_image, cudaMemcpyHostToDevice, stream));
-    preprocess_kernel_img(img_device, img.cols, img.rows, buffer_idx, INPUT_W, INPUT_H, stream);
-    buffer_idx += size_image_dst;
-
-    // Run inference
-    doInference(*context, stream, (void **) buffers, prob, BATCH_SIZE);
-    std::vector<std::vector<Yolo::Detection>> batch_res(1);
-    auto &res = batch_res[0];
-    nms(res, &prob[0], CONF_THRESH, NMS_THRESH);
-    for (auto &re: res) {
-        cv::Rect r = get_rect(img, re.bbox);
-        cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
-        cv::putText(img, std::to_string((int) re.class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2,
-                    cv::Scalar(0xFF, 0xFF, 0xFF), 2);
-        rc_msgs::detection tmp;
-        tmp.x1 = r.x;
-        tmp.y1 = r.y;
-        tmp.x2 = r.x + r.width;
-        tmp.y2 = r.y + r.height;
-        tmp.label = (int) re.class_id;
-        tmp.score = re.conf;
-        std::vector<rc_msgs::point> ps(4);
-        ps[0].x = r.x;
-        ps[0].y = r.y;
-        ps[1].x = r.x + r.width;
-        ps[1].y = r.y;
-        ps[2].x = r.x + r.width;
-        ps[2].y = r.y + r.height;
-        ps[3].x = r.x;
-        ps[3].y = r.y + r.height;
-        tmp.contours = ps;
-        Result.results.push_back(tmp);
-    }
-*/
-//copy to memory and interface for v5
 }
 
 void callback(const rc_msgs::stepConfig &config) {
@@ -311,55 +254,6 @@ shared_ptr<Yolo::Infer> init() {
 //init for v7
 
 int main(int argc, char **argv) {
-
-//init for v5
-/*
-    cudaSetDevice(DEVICE);
-
-    std::string engine_name(ENGINE_PATH);
-    engine_name += "/engines/test.engine";
-
-    std::ifstream file(engine_name, std::ios::binary);
-    if (!file.good()) {
-        std::cerr << "read " << engine_name << " error!" << std::endl;
-        return -1;
-    }
-    std::cout << "start build engine\n";
-    char *trtModelStream = nullptr;
-    size_t size = 0;
-    file.seekg(0, file.end);
-    size = file.tellg();
-    file.seekg(0, file.beg);
-    trtModelStream = new char[size];
-    assert(trtModelStream);
-    file.read(trtModelStream, size);
-    file.close();
-
-    IRuntime *runtime = createInferRuntime(gLogger);
-    assert(runtime != nullptr);
-    ICudaEngine *engine = runtime->deserializeCudaEngine(trtModelStream, size);
-    assert(engine != nullptr);
-    context = engine->createExecutionContext();
-    assert(context != nullptr);
-    delete[] trtModelStream;
-    assert(engine->getNbBindings() == 2);
-    // In order to bind the buffers, we need to know the names of the input and output tensors.
-    // Note that indices are guaranteed to be less than IEngine::getNbBindings()
-    int inputIndex = engine->getBindingIndex(INPUT_BLOB_NAME);
-    const int outputIndex = engine->getBindingIndex(OUTPUT_BLOB_NAME);
-    assert(inputIndex == 0);
-    assert(outputIndex == 1);
-    // Create GPU buffers on DEVICE
-    CUDA_CHECK(cudaMalloc((void **) &buffers[inputIndex], BATCH_SIZE * 3 * INPUT_H * INPUT_W * sizeof(float)));
-    CUDA_CHECK(cudaMalloc((void **) &buffers[outputIndex], BATCH_SIZE * OUTPUT_SIZE * sizeof(float)));
-    CUDA_CHECK(cudaStreamCreate(&stream));
-    // prepare input data cache in pinned memory
-    CUDA_CHECK(cudaMallocHost((void **) &img_host, MAX_IMAGE_INPUT_SIZE_THRESH * 3));
-    // prepare input data cache in DEVICE memory
-    CUDA_CHECK(cudaMalloc((void **) &img_device, MAX_IMAGE_INPUT_SIZE_THRESH * 3));
-    std::cout << "build ok\n";
-*/
-//init for v5
     engine = init();
     ros::init(argc, argv, "yolo_identify");
     ros::NodeHandle n;
@@ -381,18 +275,6 @@ int main(int argc, char **argv) {
     }
     beatRun = false;
     beatThread.join();
-// Release stream and buffers
-/*
-    cudaStreamDestroy(stream);
-    CUDA_CHECK(cudaFree(img_device));
-    CUDA_CHECK(cudaFreeHost(img_host));
-    CUDA_CHECK(cudaFree(buffers[inputIndex]));
-    CUDA_CHECK(cudaFree(buffers[outputIndex]));
-    Destroy the engine
-    context->destroy();
-    engine->destroy();
-    runtime->destroy();
-*/
     return 0;
 }
 
